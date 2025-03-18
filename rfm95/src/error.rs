0ccedeac03b37ecde5fd565880825bc744fc406d
Keziah Biermann <development@kizzycode.de>
2025-03-18 18:22:57 +0100

    More distinct error types

--- rfm95/src/error.rs
+++ rfm95/src/error.rs
@@ -1,54 +1,23 @@
-//! The crate's error type
+//! The crate's error types
 
 /// Creates an error
 #[macro_export]
 macro_rules! err {
-    ($kind:expr, $desc:expr) => {{
-        #[cfg(feature = "backtrace")]
-        {
-            // Create an error with rich backtrace information
-            $crate::error::Error { kind: $kind, file: file!(), line: line!(), description: $desc }
+    ($kind:tt, $desc:expr) => {{
+        $kind {
+            #[cfg(feature = "backtrace")]
+            file: file!(),
+            #[cfg(feature = "backtrace")]
+            line: line!(),
+            #[cfg(feature = "backtrace")]
+            description: $desc,
         }
-
-        #[cfg(not(feature = "backtrace"))]
-        {
-            // Create a size-optimized error
-            $crate::error::Error { kind: $kind }
-        }
-    }};
-    (eio: $desc:expr) => {{
-        err!($crate::error::ErrorKind::IoError, $desc)
-    }};
-    (etimedout: $desc:expr) => {{
-        err!($crate::error::ErrorKind::Timeout, $desc)
-    }};
-    (ebadmsg: $desc:expr) => {{
-        err!($crate::error::ErrorKind::InvalidMessage, $desc)
-    }};
-    (einval: $desc:expr) => {{
-        err!($crate::error::ErrorKind::InvalidValue, $desc)
     }};
 }
 
-/// The error kind
-#[derive(Debug, PartialEq, Eq)]
-#[repr(u8)]
-pub enum ErrorKind {
-    /// An SPI or reset-related I/O error
-    IoError,
-    /// A timeout occurred
-    Timeout,
-    /// A CRC validation failed
-    InvalidMessage,
-    /// A function argument is invalid
-    InvalidValue,
-}
-
-/// The crate's error type
-#[derive(Debug)]
-pub struct Error {
-    /// The error kind
-    pub kind: ErrorKind,
+/// An I/O error
+#[derive(Debug, Clone, Copy)]
+pub struct IoError {
     /// The file where the error was created
     #[cfg(feature = "backtrace")]
     pub file: &'static str,
@@ -58,4 +27,91 @@ pub struct Error {
     /// A human readable error description
     #[cfg(feature = "backtrace")]
     pub description: &'static str,
+}
+
+/// A timeout error
+#[derive(Debug, Clone, Copy)]
+pub struct TimeoutError {
+    /// The file where the error was created
+    #[cfg(feature = "backtrace")]
+    pub file: &'static str,
+    /// The line at which the error was created
+    #[cfg(feature = "backtrace")]
+    pub line: u32,
+    /// A human readable error description
+    #[cfg(feature = "backtrace")]
+    pub description: &'static str,
+}
+
+/// A CRC-validation or format error
+#[derive(Debug, Clone, Copy)]
+pub struct InvalidMessageError {
+    /// The file where the error was created
+    #[cfg(feature = "backtrace")]
+    pub file: &'static str,
+    /// The line at which the error was created
+    #[cfg(feature = "backtrace")]
+    pub line: u32,
+    /// A human readable error description
+    #[cfg(feature = "backtrace")]
+    pub description: &'static str,
+}
+
+/// An invalid-argument error
+#[derive(Debug, Clone, Copy)]
+pub struct InvalidArgumentError {
+    /// The file where the error was created
+    #[cfg(feature = "backtrace")]
+    pub file: &'static str,
+    /// The line at which the error was created
+    #[cfg(feature = "backtrace")]
+    pub line: u32,
+    /// A human readable error description
+    #[cfg(feature = "backtrace")]
+    pub description: &'static str,
+}
+
+/// An RX- or TX-start error
+#[derive(Debug, Clone, Copy)]
+pub enum RxTxStartError {
+    /// A low-level SPI or reset-related I/O error
+    WireIoError(IoError),
+    /// An invalid-argument error
+    InvalidArgumentError(InvalidArgumentError),
+}
+impl From<IoError> for RxTxStartError {
+    fn from(error: IoError) -> Self {
+        Self::WireIoError(error)
+    }
+}
+impl From<InvalidArgumentError> for RxTxStartError {
+    fn from(error: InvalidArgumentError) -> Self {
+        Self::InvalidArgumentError(error)
+    }
+}
+
+/// An RX-completion specific error
+#[derive(Debug, Clone, Copy)]
+pub enum RxCompleteError {
+    /// A low-level SPI or reset-related I/O error
+    WireIoError(IoError),
+    /// A timeout error
+    TimeoutError(TimeoutError),
+    /// A CRC-validation or format error
+    InvalidMessageError(InvalidMessageError),
+}
+impl From<IoError> for RxCompleteError {
+    fn from(error: IoError) -> Self {
+        Self::WireIoError(error)
+    }
+}
+impl From<TimeoutError> for RxCompleteError {
+    fn from(error: TimeoutError) -> Self {
+        Self::TimeoutError(error)
+    }
+}
+impl From<InvalidMessageError> for RxCompleteError {
+    fn from(error: InvalidMessageError) -> Self {
+        Self::InvalidMessageError(error)
+    }
 }
